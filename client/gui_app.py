6dece4e85c69b8ade246fb4146b2bc5700824f91
Sebas21EC <sebastiantabango@gmail.com>
2022-09-23 12:53:20 -0500

    Tabla de datos y diseño completo de la interfaz

--- client/gui_app.py
+++ client/gui_app.py
@@ -1,6 +1,8 @@
 from cgitb import enable
 from math import fabs
 import tkinter as tk
+from tkinter import ttk
+
 ##Funcion para crear el menu
 def barra_menu (root):
     barra_menu=tk.Menu(root)
@@ -26,6 +28,7 @@ class Frame(tk.Frame):
         #self.config( bg='gray')
         self.campos_peliculas()
         self.deshabilitar_campos()
+        self.tabla_datos()
     
     ##Crear los elementos del frame.
     def campos_peliculas(self):
@@ -43,33 +46,40 @@ class Frame(tk.Frame):
         self.label_genero.grid(row=2,column=0,padx=10,pady=10)
 
         #campos de entrada
-        self.entry_nombre=tk.Entry(self)
+        self.nombre=tk.StringVar()
+        self.entry_nombre=tk.Entry(self,textvariable=self.nombre)
         self.entry_nombre.config(width=50,font=('Arial',12))
         self.entry_nombre.grid(row=0,column=1,padx=10,pady=10,columnspan=2)
 
-        self.entry_duracion=tk.Entry(self)
+        self.duracion=tk.StringVar()
+        self.entry_duracion=tk.Entry(self,textvariable=self.duracion)
         self.entry_duracion.config(width=50,font=('Arial',12))
         self.entry_duracion.grid(row=1,column=1,padx=10,pady=10,columnspan=2)
 
-        self.entry_genero=tk.Entry(self)
+        self.genero=tk.StringVar()
+        self.entry_genero=tk.Entry(self,textvariable=self.genero)
         self.entry_genero.config(width=50,font=('Arial',12))
         self.entry_genero.grid(row=2,column=1,padx=10,pady=10,columnspan=2)
 
         #botones
         self.btn_nuevo=tk.Button(self,text="Nuevo",command=self.habilitar_campos)
         self.btn_nuevo.config(width=20,font=('Arial',12,'bold'),fg='white',background='#04B404', curso='hand2',activebackground='#35BD6F')
-        self.btn_nuevo.grid(row=4,column=0)
+        self.btn_nuevo.grid(row=3,column=0)
 
-        self.btn_guardar=tk.Button(self,text="Guardar")
+        self.btn_guardar=tk.Button(self,text="Guardar",command=self.guardar_datos)
         self.btn_guardar.config(width=20,font=('Arial',12,'bold'),fg='white',background='blue', curso='hand2',activebackground='cyan')
-        self.btn_guardar.grid(row=4,column=1)
+        self.btn_guardar.grid(row=3,column=1)
 
         self.btn_cancelar=tk.Button(self,text="Cancelar",command=self.deshabilitar_campos)
         self.btn_cancelar.config(width=20,font=('Arial',12,'bold'),fg='white',background='orange', curso='hand2',activebackground='brown')
-        self.btn_cancelar.grid(row=4,column=2)
+        self.btn_cancelar.grid(row=3,column=2)
 
         # Metodo para habilitar los campos
     def habilitar_campos(self):
+        self.nombre.set('')
+        self.duracion.set('')
+        self.genero.set('')
+
         self.entry_nombre.config(state='normal')
         self.entry_duracion.config(state='normal')
         self.entry_genero.config(state='normal')
@@ -78,6 +88,10 @@ class Frame(tk.Frame):
         self.btn_cancelar.config(state='normal')
 
     def deshabilitar_campos(self):
+        self.nombre.set('')
+        self.duracion.set('')
+        self.genero.set('')
+
         self.entry_nombre.config(state='disabled')
         self.entry_duracion.config(state='disabled')
         self.entry_genero.config(state='disabled')
@@ -85,4 +99,31 @@ class Frame(tk.Frame):
         self.btn_guardar.config(state='disabled')
         self.btn_cancelar.config(state='disabled')
 
+    def guardar_datos(self):
+
+
+
+        self.deshabilitar_campos()
+
+    #Diseñar la tabla de datos
+    def tabla_datos(self):
+        self.table=ttk.Treeview(self,columns=('Nombre','Duración','Género'))
+        self.table.grid(row=4,column=0,columnspan=4)
+
+        self.table.heading('#0',text='ID')
+        self.table.heading('#1',text='NOMBRE')
+        self.table.heading('#2',text='DURACIÓN')
+        self.table.heading('#3',text='GÉNERO')
+
+        #INSERTAR DAOTS EN LA TABLA
+        self.table.insert('',0,text=1,values=('Table 19','2,55','Comedia'))
         
+
+        self.btn_editar=tk.Button(self,text="Editar")
+        self.btn_editar.config(width=20,font=('Arial',12,'bold'),fg='white',background='#04B404', curso='hand2',activebackground='#35BD6F')
+        self.btn_editar.grid(row=5,column=0)
+
+        self.btn_eliminar=tk.Button(self,text="Eliminar")
+        self.btn_eliminar.config(width=20,font=('Arial',12,'bold'),fg='white',background='orange', curso='hand2',activebackground='brown')
+        self.btn_eliminar.grid(row=5,column=1)
+    
