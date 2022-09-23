cb4617ba1e69aa8a1f197cecfec410a171d756b4
Sebas21EC <sebastiantabango@gmail.com>
2022-09-23 12:30:51 -0500

    Habilitar y deshabilitar campos y botones

--- client/gui_app.py
+++ client/gui_app.py
@@ -25,6 +25,7 @@ class Frame(tk.Frame):
         self.pack()
         #self.config( bg='gray')
         self.campos_peliculas()
+        self.deshabilitar_campos()
     
     ##Crear los elementos del frame.
     def campos_peliculas(self):
@@ -43,19 +44,19 @@ class Frame(tk.Frame):
 
         #campos de entrada
         self.entry_nombre=tk.Entry(self)
-        self.entry_nombre.config(width=50,state='disabled',font=('Arial',12))
-        self.entry_nombre.grid(row=0,column=1,padx=10,pady=10)
+        self.entry_nombre.config(width=50,font=('Arial',12))
+        self.entry_nombre.grid(row=0,column=1,padx=10,pady=10,columnspan=2)
 
         self.entry_duracion=tk.Entry(self)
-        self.entry_duracion.config(width=50,state='disabled',font=('Arial',12))
-        self.entry_duracion.grid(row=1,column=1,padx=10,pady=10)
+        self.entry_duracion.config(width=50,font=('Arial',12))
+        self.entry_duracion.grid(row=1,column=1,padx=10,pady=10,columnspan=2)
 
         self.entry_genero=tk.Entry(self)
-        self.entry_genero.config(width=50,state='disabled',font=('Arial',12))
-        self.entry_genero.grid(row=2,column=1,padx=10,pady=10)
+        self.entry_genero.config(width=50,font=('Arial',12))
+        self.entry_genero.grid(row=2,column=1,padx=10,pady=10,columnspan=2)
 
         #botones
-        self.btn_nuevo=tk.Button(self,text="Nuevo")
+        self.btn_nuevo=tk.Button(self,text="Nuevo",command=self.habilitar_campos)
         self.btn_nuevo.config(width=20,font=('Arial',12,'bold'),fg='white',background='#04B404', curso='hand2',activebackground='#35BD6F')
         self.btn_nuevo.grid(row=4,column=0)
 
@@ -63,7 +64,25 @@ class Frame(tk.Frame):
         self.btn_guardar.config(width=20,font=('Arial',12,'bold'),fg='white',background='blue', curso='hand2',activebackground='cyan')
         self.btn_guardar.grid(row=4,column=1)
 
-        self.btn_cancelar=tk.Button(self,text="Cancelar")
+        self.btn_cancelar=tk.Button(self,text="Cancelar",command=self.deshabilitar_campos)
         self.btn_cancelar.config(width=20,font=('Arial',12,'bold'),fg='white',background='orange', curso='hand2',activebackground='brown')
         self.btn_cancelar.grid(row=4,column=2)
 
+        # Metodo para habilitar los campos
+    def habilitar_campos(self):
+        self.entry_nombre.config(state='normal')
+        self.entry_duracion.config(state='normal')
+        self.entry_genero.config(state='normal')
+
+        self.btn_guardar.config(state='normal')
+        self.btn_cancelar.config(state='normal')
+
+    def deshabilitar_campos(self):
+        self.entry_nombre.config(state='disabled')
+        self.entry_duracion.config(state='disabled')
+        self.entry_genero.config(state='disabled')
+
+        self.btn_guardar.config(state='disabled')
+        self.btn_cancelar.config(state='disabled')
+
+        
