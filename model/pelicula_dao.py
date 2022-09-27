7fd46552c7df0b9a0acc1e2513e1c551c12d5318
Sebas21EC <sebastiantabango@gmail.com>
2022-09-26 19:36:11 -0500

    manejo de excepciones producidas al ejecutar sentencias sql

--- model/pelicula_dao.py
+++ model/pelicula_dao.py
@@ -1,4 +1,5 @@
 from .conexion_db import Conexion_DB
+from tkinter import messagebox
 
 def crear_tabla():
     conexion=Conexion_DB()
@@ -11,12 +12,30 @@ def crear_tabla():
         genero VARCHAR(50),
         PRIMARY KEY(id_pelicula AUTOINCREMENT)
     )'''
+    titulo = 'Crear Registro'
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar_db()
+        mensaje = 'Se creó la tabla en la base de datos'
 
-    conexion.cursor.execute(sql)
-    conexion.cerrar_db()
+    except:
+        mensaje = 'La tabla ya se encuentra creada'
+
+    messagebox.showinfo(titulo,mensaje)
+    
 
 def eliminar_tabla():
     conexion=Conexion_DB()
     sql = ' DROP TABLE peliculas_tb'
     conexion.cursor.execute(sql)
     conexion.cerrar_db()
+    titulo = 'Borrar Registro'
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar_db()
+        mensaje = 'Se eliminó la tabla en la base de datos'
+
+    except:
+        mensaje = 'No existe esta tabla para eliminar'
+    
+    messagebox.showerror(titulo,mensaje)
